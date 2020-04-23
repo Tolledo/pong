1d98ac1f6468a2f7c9d56adb26a1044e03e89b71
Tolledo <ps.tolledo@gmail.com>
2020-04-22 22:24:30 -0700

    Add scoring board

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use amethyst::{
         RenderingBundle,
     },
     utils::application_root_dir,
+    ui::{RenderUi, UiBundle},
     input::{InputBundle, StringBindings},
 };
 
@@ -30,6 +31,7 @@ fn main() -> amethyst::Result<()> {
         // Add the transform bundle which handles tracking entity positions
         .with_bundle(TransformBundle::new())?
         .with_bundle(input_bundle)?
+        .with_bundle(UiBundle::<StringBindings>::new())?
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
                 // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
@@ -38,11 +40,13 @@ fn main() -> amethyst::Result<()> {
                         .with_clear([0.00196, 0.23726, 0.21765, 1.0]),
                 )
                 // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
-                .with_plugin(RenderFlat2D::default()),
+                .with_plugin(RenderFlat2D::default())
+                .with_plugin(RenderUi::default()),
         )?
         .with(system::PaddleSystem, "paddle_system", &["input_system"])
         .with(system::MoveBallsSystem, "ball_system", &[])
-        .with(system::BounceSystem, "collision_system", &["paddle_system", "ball_system"]);
+        .with(system::BounceSystem, "collision_system", &["paddle_system", "ball_system"])
+        .with(system::WinnerSystem, "winner_system", &["ball_system"]);
 
     let mut game = Application::new(assets_path, Pong::default(), game_data)?;
     game.run();
